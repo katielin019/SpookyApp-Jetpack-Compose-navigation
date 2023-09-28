0e475303cbc25ffe59ae6d998d09356b0757311c
Katie Lin <katielin019@gmail.com>
2023-09-28 09:49:00 -0400

    Working on main architecture scaffolding

--- app/src/main/java/com/zk/spookynavigation/MainActivity.kt
+++ app/src/main/java/com/zk/spookynavigation/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.zk.spookynavigation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
+import com.zk.spookynavigation.ViewModels.Library
+import com.zk.spookynavigation.Views.TabLayout
 import com.zk.spookynavigation.ui.NavigationTestTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val library: Library by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationTestTheme {
-                MainScreen()
+                TabLayout(viewModel = library)
             }
         }
     }
